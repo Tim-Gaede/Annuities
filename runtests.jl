5ae94966f9ec2a40c75f8127c884ec4db843319f
Tim-Gaede <TimGaede@gmail.com>
2019-10-17 12:30:50 -0400

    Update runtests.jl

--- runtests.jl
+++ runtests.jl
@@ -1,11 +1,5 @@
 using Test
 
-include("annuities.jl")
-
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
-
-println("\n"^3, "-"^60, "\n")
-
 @testset "5.3% interest over 20 payments.............." begin
     @test annuityRatio(5.3, 20) ≈ 1.7066994798369612
 end
@@ -16,8 +10,8 @@ println("\n")
 end
 println("\n")
 
-@testset "-5.0% interest over 10 payments............." begin
-    @test annuityRatio(-5.0, 10) ≈ 0.8025261215232422
+@testset "7.5% interest over 10 payments............." begin
+    @test annuityRatio(7.5, 10) ≈ 1.4147087495529487
 end
 println("\n")
 
@@ -27,8 +21,8 @@ println("\n")
 end
 println("\n")
 
-@testset "Negative 150% interest throws DomainError..." begin
-    @test_throws DomainError annuityRatio(-150.0, 25)
+@testset "Negative interest throws DomainError......." begin
+    @test_throws DomainError annuityRatio(-2.0, 25)
 end
 println("\n")
 
